fd7cb7da289eaf89dbb9c798b9e9a51ef7681799
PetarKebakoski <pkebakoski@gmail.com>
2022-02-10 13:27:49 +0100

    https://github.com/PetarKebakoski/deployAngularCompanyManager

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HolidaysComponent } from './holidays/holidays.component';
+import { HomeComponent } from './home/home.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { TimeCardComponent } from './time-card/time-card.component';
 
 const routes: Routes = [
-  {path: '', component: LoginPageComponent}
+  { path: 'login', component: LoginPageComponent },
+  { path: '', component: HomeComponent },
+  { path: 'statistics', component: StatisticsComponent },
+  { path: 'holidays', component: HolidaysComponent },
+  { path: 'time-card', component: TimeCardComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { HomeComponent } from './home/home.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { HolidaysComponent } from './holidays/holidays.component';
+import { TimeCardComponent } from './time-card/time-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    HomeComponent,
+    StatisticsComponent,
+    HolidaysComponent,
+    TimeCardComponent
   ],
   imports: [
     BrowserModule,
